8a89923720eaf657b31731b235262969e5ac6e28
jadhavpradip90 <jadhavpradip90@gmail.com>
2018-04-01 16:03:29 +0530

    Redux Middleware

--- src/app/index.js
+++ src/app/index.js
@@ -30,7 +30,8 @@ class App extends React.Component {
 
 render(<App />, window.document.getElementById('app'));*/
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger'
 
 
 const mathReducer = (state = {
@@ -77,10 +78,19 @@ const userReducer = (state = {
     return state;
 };
 
-const store = createStore(combineReducers({mathReducer, userReducer}));
+const myLogger = (store) => (next) => (action) => {
+    console.log("Logged Action :: ", action);
+    next(action);
+};
+
+const store = createStore(
+    combineReducers({mathReducer, userReducer}),
+    {},
+    applyMiddleware(createLogger())
+);
 
 store.subscribe(() => {
-    console.log("Store updated :: ", store.getState());
+    //console.log("Store updated :: ", store.getState());
 })
 
 store.dispatch({
